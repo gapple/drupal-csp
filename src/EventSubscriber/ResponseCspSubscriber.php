a84c2c6b7c5bd39461d201279e43b155cb6e711e
Geoff Appleby <geoff@gapple.ca>
2017-08-07 22:41:31 -0700

    Issue #2895242 by gapple: Create default policy from Libraries

--- src/EventSubscriber/ResponseCspSubscriber.php
+++ src/EventSubscriber/ResponseCspSubscriber.php
@@ -2,8 +2,13 @@
 
 namespace Drupal\csp\EventSubscriber;
 
+use Drupal\Core\Asset\LibraryDiscovery;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\csp\Csp;
+use GuzzleHttp\Psr7\Uri;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
@@ -21,13 +26,55 @@ class ResponseCspSubscriber implements EventSubscriberInterface {
   protected $configFactory;
 
   /**
+   * The Library Discovery service.
+   *
+   * @var \Drupal\Core\Asset\LibraryDiscovery
+   */
+  protected $libraryDiscovery;
+
+  /**
+   * The cache bin.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
+
+  /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Arrays of hosts, keyed by resource type.
+   *
+   * @var array
+   */
+  protected $hosts;
+
+  /**
    * Constructs a new ResponseSubscriber object.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    *   Config Factory service.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
+   *   The cache bin.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The Module Handler Interface.
+   * @param \Drupal\Core\Asset\LibraryDiscovery $libraryDiscovery
+   *   The Library Discovery Collector service.
    */
-  public function __construct(ConfigFactoryInterface $configFactory) {
+  public function __construct(
+    ConfigFactoryInterface $configFactory,
+    CacheBackendInterface $cache,
+    ModuleHandlerInterface $moduleHandler,
+    LibraryDiscovery $libraryDiscovery
+  ) {
     $this->configFactory = $configFactory;
+    $this->cache = $cache;
+    $this->moduleHandler = $moduleHandler;
+    $this->libraryDiscovery = $libraryDiscovery;
   }
 
   /**
@@ -56,6 +103,15 @@ class ResponseCspSubscriber implements EventSubscriberInterface {
 
     $policy->setDirective('default-src', [Csp::POLICY_SELF]);
 
+    if (($scriptHosts = $this->getHosts('script'))) {
+      $policy->setDirective('script-src', [Csp::POLICY_SELF]);
+      $policy->appendDirective('script-src', $scriptHosts);
+    }
+    if (($styleHosts = $this->getHosts('style'))) {
+      $policy->setDirective('style-src', [Csp::POLICY_SELF]);
+      $policy->appendDirective('style-src', $styleHosts);
+    }
+
     // IE9 only supports up to 31 stylesheets on the page, so if there are more
     // CssCollectionRenderer outputs them inline as @import statements instead.
     // Since checking the actual number of stylesheets included on the page is
@@ -64,10 +120,119 @@ class ResponseCspSubscriber implements EventSubscriberInterface {
     // @see CssCollectionRenderer::render()
     // @see HtmlResponseAttachmentsProcessor::processAssetLibraries()
     if (defined('MAINTENANCE_MODE') || !$this->configFactory->get('system.performance')->get('css.preprocess')) {
-      $policy->appendDirective('style-src', [Csp::POLICY_SELF, Csp::POLICY_UNSAFE_INLINE]);
+      if (!$styleHosts) {
+        $policy->appendDirective('style-src', [Csp::POLICY_SELF]);
+      }
+      $policy->appendDirective('style-src', [Csp::POLICY_UNSAFE_INLINE]);
     }
 
     $response->headers->set($policy->getHeaderName(), $policy->getHeaderValue());
+  }
+
+  /**
+   * Retrieve and cache hosts from library definitions.
+   */
+  protected function parseLibraryHosts() {
+    $moduleList = $this->moduleHandler->getModuleList();
+
+    $scriptHosts = [];
+    $styleHosts = [];
+
+    foreach ($moduleList as $extensionName => $extensionInfo) {
+      $moduleLibraries = $this->libraryDiscovery->getLibrariesByExtension($extensionName);
+
+      foreach ($moduleLibraries as $libraryName => $libraryInfo) {
+        foreach ($libraryInfo['js'] as $jsInfo) {
+          if ($jsInfo['type'] == 'external') {
+            $scriptHosts[] = $this->getHostFromUri($jsInfo['data']);
+          }
+        }
+        foreach ($libraryInfo['css'] as $cssInfo) {
+          if ($cssInfo['type'] == 'external') {
+            $styleHosts[] = $this->getHostFromUri($cssInfo['data']);
+          }
+        }
+      }
+    }
+
+    sort($scriptHosts);
+    sort($styleHosts);
+
+    // TODO reduce to minimal set
+    // e.g.
+    // - Wildcards and matching subdomains (*.example.com, sub.example.com)
+    // - Protocols (example.com, https://example.com)
+    $this->setHosts('script', array_unique($scriptHosts));
+    $this->setHosts('style', array_unique($styleHosts));
+  }
+
+  /**
+   * Cache a set of hosts by resource type.
+   *
+   * @param string $type
+   *   The resource type, 'script' or 'style'.
+   * @param array $hosts
+   *   An array of hosts.
+   */
+  protected function setHosts($type, array $hosts) {
+    $this->hosts[$type] = $hosts;
+
+    $this->cache->set('csp:hosts:' . $type, $hosts, Cache::PERMANENT, ['library_info']);
+  }
+
+  /**
+   * Retrieve a set of hosts by resource type.
+   *
+   * @param string $type
+   *   The resource type, 'script' or 'style'.
+   *
+   * @return array
+   *   An array of hosts.
+   *
+   * @throws \Exception
+   */
+  protected function getHosts($type) {
+
+    if (!isset($this->hosts[$type])) {
+      $cacheData = $this->cache->get('csp:hosts:' . $type);
+
+      if ($cacheData) {
+        $this->hosts[$type] = $cacheData->data;
+      }
+    }
+
+    if (!isset($this->hosts[$type])) {
+      $this->parseLibraryHosts();
+    }
+
+    if (!isset($this->hosts[$type])) {
+      throw new \Exception("Host type not available.");
+    }
+
+    return $this->hosts[$type];
+  }
+
+  /**
+   * Get host info from a URI.
+   *
+   * @param string $uri
+   *   The URI.
+   *
+   * @return string
+   *   The host info.
+   */
+  protected function getHostFromUri($uri) {
+    $uri = new Uri($uri);
+    $host = $uri->getHost();
+
+    // Only include scheme if restricted to HTTPS.
+    if ($uri->getScheme() === 'https') {
+      $host = 'https://' . $host;
+    }
+    if (($port = $uri->getPort())) {
+      $host .= ':' . $port;
+    }
+    return $host;
   }
 
 }
--- tests/src/Unit/EventSubscriber/ResponseCspSubscriberTest.php
+++ tests/src/Unit/EventSubscriber/ResponseCspSubscriberTest.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\csp\Unit\EventSubscriber;
 
+use Drupal\Core\Asset\LibraryDiscovery;
+use Drupal\Core\Cache\MemoryBackend;
+use Drupal\Core\Extension\ModuleHandler;
 use Drupal\Core\Render\HtmlResponse;
 use Drupal\csp\EventSubscriber\ResponseCspSubscriber;
 use Drupal\Tests\UnitTestCase;
@@ -15,28 +18,81 @@ use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 class ResponseCspSubscriberTest extends UnitTestCase {
 
   /**
+   * Mock HTTP Response.
+   *
+   * @var \Drupal\Core\Render\HtmlResponse|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $response;
+
+  /**
+   * Mock Response Event.
+   *
+   * @var \Symfony\Component\HttpKernel\Event\FilterResponseEvent|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $event;
+
+  /**
+   * Memory Cache backend.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  protected $cache;
+
+  /**
+   * Mock Module Handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $moduleHandler;
+
+  /**
+   * Mock Library Discovery.
+   *
+   * @var \Drupal\Core\Asset\LibraryDiscoveryInterface|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $libraryDiscovery;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->response = $this->getMockBuilder(HtmlResponse::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $this->response->headers = $this->getMockBuilder(ResponseHeaderBag::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    /** @var \Symfony\Component\HttpKernel\Event\FilterResponseEvent|\PHPUnit_Framework_MockObject_MockObject $event */
+    $this->event = $this->getMockBuilder(FilterResponseEvent::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $this->event->expects($this->any())
+      ->method('isMasterRequest')
+      ->willReturn(TRUE);
+    $this->event->expects($this->any())
+      ->method('getResponse')
+      ->willReturn($this->response);
+
+    $this->cache = new MemoryBackend();
+
+    $this->moduleHandler = $this->getMockBuilder(ModuleHandler::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $this->libraryDiscovery = $this->getMockBuilder(LibraryDiscovery::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+  }
+
+  /**
    * Check the policy with CSS optimization disabled.
    *
    * @covers ::onKernelResponse
    */
   public function testUnoptimizedResponse() {
-    $response = $this->getMockBuilder(HtmlResponse::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $response->headers = $this->getMockBuilder(ResponseHeaderBag::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-
-    /** @var \Symfony\Component\HttpKernel\Event\FilterResponseEvent|\PHPUnit_Framework_MockObject_MockObject $event */
-    $event = $this->getMockBuilder(FilterResponseEvent::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $event->expects($this->any())
-      ->method('isMasterRequest')
-      ->willReturn(TRUE);
-    $event->expects($this->any())
-      ->method('getResponse')
-      ->willReturn($response);
 
     /** @var \Drupal\Core\Config\ConfigFactoryInterface|\PHPUnit_Framework_MockObject_MockObject $configFactory */
     $configFactory = $this->getConfigFactoryStub([
@@ -45,16 +101,20 @@ class ResponseCspSubscriberTest extends UnitTestCase {
       ],
     ]);
 
-    $subscriber = new ResponseCspSubscriber($configFactory);
+    $this->moduleHandler->expects($this->any())
+      ->method('getModuleList')
+      ->willReturn([]);
 
-    $response->headers->expects($this->once())
+    $subscriber = new ResponseCspSubscriber($configFactory, $this->cache, $this->moduleHandler, $this->libraryDiscovery);
+
+    $this->response->headers->expects($this->once())
       ->method('set')
       ->with(
         $this->equalTo('Content-Security-Policy-Report-Only'),
         $this->equalTo("default-src 'self'; style-src 'self' 'unsafe-inline'")
       );
 
-    $subscriber->onKernelResponse($event);
+    $subscriber->onKernelResponse($this->event);
   }
 
   /**
@@ -63,23 +123,6 @@ class ResponseCspSubscriberTest extends UnitTestCase {
    * @covers ::onKernelResponse
    */
   public function testOptimizedResponse() {
-    $response = $this->getMockBuilder(HtmlResponse::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $response->headers = $this->getMockBuilder(ResponseHeaderBag::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-
-    /** @var \Symfony\Component\HttpKernel\Event\FilterResponseEvent|\PHPUnit_Framework_MockObject_MockObject $event */
-    $event = $this->getMockBuilder(FilterResponseEvent::class)
-      ->disableOriginalConstructor()
-      ->getMock();
-    $event->expects($this->any())
-      ->method('isMasterRequest')
-      ->willReturn(TRUE);
-    $event->expects($this->any())
-      ->method('getResponse')
-      ->willReturn($response);
 
     /** @var \Drupal\Core\Config\ConfigFactoryInterface|\PHPUnit_Framework_MockObject_MockObject $configFactory */
     $configFactory = $this->getConfigFactoryStub([
@@ -88,16 +131,20 @@ class ResponseCspSubscriberTest extends UnitTestCase {
       ],
     ]);
 
-    $subscriber = new ResponseCspSubscriber($configFactory);
+    $this->moduleHandler->expects($this->any())
+      ->method('getModuleList')
+      ->willReturn([]);
 
-    $response->headers->expects($this->once())
+    $subscriber = new ResponseCspSubscriber($configFactory, $this->cache, $this->moduleHandler, $this->libraryDiscovery);
+
+    $this->response->headers->expects($this->once())
       ->method('set')
       ->with(
         $this->equalTo('Content-Security-Policy-Report-Only'),
         $this->equalTo("default-src 'self'")
       );
 
-    $subscriber->onKernelResponse($event);
+    $subscriber->onKernelResponse($this->event);
   }
 
 }
