a73caad7a0180bfd86137969a601337570a45f4b
Geoff Appleby <geoff@gapple.ca>
2018-02-25 21:55:33 -0800

    Policy config form structure

--- src/Form/CspSettingsForm.php
+++ src/Form/CspSettingsForm.php
@@ -5,6 +5,7 @@ namespace Drupal\csp\Form;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\csp\Csp;
 
 /**
  * Form for editing Content Security Policy module settings.
@@ -33,12 +34,7 @@ class CspSettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('csp.settings');
 
-    $form['enforce'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Enforce'),
-      '#description' => $this->t('Enable policy enforcement.  If disabled, policy is set to report-only.'),
-      '#default_value' => $config->get('enforce'),
-    ];
+    $form['#attached']['library'][] = 'csp/admin';
 
     $form['report'] = [
       '#type' => 'fieldset',
@@ -46,7 +42,7 @@ class CspSettingsForm extends ConfigFormBase {
       '#tree' => TRUE,
     ];
     $form['report']['handler'] = [
-      '#type' => 'select',
+      '#type' => 'radios',
       '#title' => $this->t('Handler'),
       '#options' => [
         'csp-module' => $this->t('Internal'),
@@ -104,6 +100,162 @@ class CspSettingsForm extends ConfigFormBase {
         ],
       ],
     ];
+
+    $form['policies'] = [
+      '#type' => 'vertical_tabs',
+      '#title' => $this->t('Policies'),
+    ];
+
+    // Exclude some directives
+    // - Reporting directives have dedicated fields elsewhere in the form.
+    // - 'referrer' is deprecated in favour of the Referrer-Policy header, and
+    //   not supported in most browsers.
+    $directives = array_diff(
+      Csp::getDirectiveNames(),
+      ['report-uri', 'report-to', 'referrer']
+    );
+    // These directives may have custom options instead of sources.
+    $customOptionDirectives = [
+      'plugin-types',
+      'sandbox',
+      'block-all-mixed-content',
+      'require-sri-for',
+      'upgrade-insecure-requests',
+    ];
+    // Directives which do not support unsafe flags.
+    $noUnsafe = [
+      'frame-ancestors',
+    ];
+
+    $policyTypes = [
+      'report-only' => $this->t('Report Only'),
+      'enforced' => $this->t('Enforced'),
+    ];
+    foreach ($policyTypes as $policyTypeKey => $policyTypeName) {
+      $form[$policyTypeKey] = [
+        '#type' => 'details',
+        '#title' => $policyTypeName,
+        '#group' => 'policies',
+        '#tree' => TRUE,
+      ];
+
+      $form[$policyTypeKey]['enable'] = [
+        '#type' => 'checkbox',
+        '#title' => $this->t("Enable '@type'", ['@type' => $policyTypeName]),
+      ];
+
+      $form[$policyTypeKey]['directives'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Directives'),
+        '#description_display' => 'before',
+        '#tree' => TRUE,
+        '#states' => [
+          'visible' => [
+            ':input[name="' . $policyTypeKey . '[enable]"]' => ['checked' => TRUE],
+          ],
+        ],
+      ];
+
+      foreach ($directives as $directive) {
+        $form[$policyTypeKey]['directives'][$directive] = [
+          '#type' => 'container',
+        ];
+        $form[$policyTypeKey]['directives'][$directive]['enable'] = [
+          '#type' => 'checkbox',
+          '#title' => $directive,
+        ];
+        $form[$policyTypeKey]['directives'][$directive]['options'] = [
+          '#type' => 'container',
+          '#states' => [
+            'visible' => [
+              ':input[name="' . $policyTypeKey . '[directives][' . $directive . '][enable]"]' => ['checked' => TRUE],
+            ],
+          ],
+        ];
+
+        if (in_array($directive, $customOptionDirectives)) {
+          continue;
+        }
+
+        $form[$policyTypeKey]['directives'][$directive]['options']['base'] = [
+          '#type' => 'radios',
+          '#parents' => [$policyTypeKey, 'directives', $directive, 'base'],
+          '#options' => [
+            'self' => "Self",
+            'none' => "None",
+            'any' => "Any",
+            '' => '<em>n/a</em>',
+          ],
+          '#default_value' => 'self',
+        ];
+
+        if (!in_array($directive, $noUnsafe)) {
+          // States currently don't work on checkboxes elements, so need to be
+          // applied to a wrapper.
+          // @see https://www.drupal.org/project/drupal/issues/994360
+          $form[$policyTypeKey]['directives'][$directive]['options']['flags_wrapper'] = [
+            '#type' => 'container',
+            '#states' => [
+              'visible' => [
+                [':input[name="' . $policyTypeKey . '[directives][' . $directive . '][base]"]' => ['!value' => 'none']],
+              ],
+            ],
+          ];
+          $form[$policyTypeKey]['directives'][$directive]['options']['flags_wrapper']['flags'] = [
+            '#type' => 'checkboxes',
+            '#parents' => [$policyTypeKey, 'directives', $directive, 'flags'],
+            '#options' => [
+              'unsafe-inline' => "<code>'unsafe-inline'</code>",
+              'unsafe-eval' => "<code>'unsafe-eval'</code>",
+            ],
+            '#default_value' => [],
+          ];
+        }
+        $form[$policyTypeKey]['directives'][$directive]['options']['sources'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Additional Sources'),
+          '#description' => $this->t('Additional domains or protocols to allow for this directive.'),
+          '#parents' => [$policyTypeKey, 'directives', $directive, 'sources'],
+          '#states' => [
+            'visible' => [
+              [':input[name="' . $policyTypeKey . '[directives][' . $directive . '][base]"]' => ['!value' => 'none']],
+            ],
+          ],
+        ];
+      }
+
+      $form[$policyTypeKey]['directives']['plugin-types']['options']['mime-types'] = [
+        '#type' => 'textfield',
+        '#parents' => [$policyTypeKey, 'directives', 'plugin-types', 'mime-types'],
+        '#title' => $this->t('MIME Types'),
+      ];
+
+      $form[$policyTypeKey]['directives']['sandbox']['options']['values'] = [
+        '#type' => 'checkboxes',
+        '#parents' => [$policyTypeKey, 'directives', 'sandbox', 'values'],
+        '#options' => [
+          'allow-forms' => '<code>allow-forms</code>',
+          'allow-modals' => '<code>allow-modals</code>',
+          'allow-orientation-lock' => '<code>allow-orientation-lock</code>',
+          'allow-pointer-lock' => '<code>allow-pointer-lock</code>',
+          'allow-popups' => '<code>allow-popups</code>',
+          'allow-popups-to-escape-sandbox' => '<code>allow-popups-to-escape-sandbox</code>',
+          'allow-presentation' => '<code>allow-presentation</code>',
+          'allow-same-origin' => '<code>allow-same-origin</code>',
+          'allow-scripts' => '<code>allow-scripts</code>',
+          'allow-top-navigation' => '<code>allow-top-navigation</code>',
+        ],
+      ];
+
+      $form[$policyTypeKey]['directives']['require-sri-for']['options']['directives'] = [
+        '#type' => 'checkboxes',
+        '#parents' => [$policyTypeKey, 'directives', 'require-sri-for', 'directives'],
+        '#options' => [
+          'script' => '<code>script</code>',
+          'style' => '<code>style</code>',
+        ],
+      ];
+    }
 
     return parent::buildForm($form, $form_state);
   }
